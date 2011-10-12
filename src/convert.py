312cc04725f04002837384c48e0d48b77183e460
Andrea Grandi <muffo87@gmail.com>
2011-10-11 19:56:49 -0700

    First working commit New evaluating system for quantity values Lot of bug fixes in unit parsing Deepcopy of UoM

--- src/convert.py
+++ src/convert.py
@@ -25,92 +25,53 @@
 
 from pyparsing import *
 from itertools import izip
+import copy
+import sys
 
+    
 
-class UnitsOfMeasurement:
+class UnitOfMeasurement:
     def __init__(self, scale=1, dimensions={}):
         self.scale = scale
         self.dimensions = dimensions
      
     
     def __mul__(self, x):
-        result = UnitsOfMeasurement(self.scale * x.scale)
+        result = UnitOfMeasurement(self.scale * x.scale)
         result.dimensions = dict( (n, self.dimensions.get(n, 0)+x.dimensions.get(n, 0)) 
                                   for n in set(self.dimensions)|set(x.dimensions) )
         return result
             
             
     def __div__(self, x):
-        result = UnitsOfMeasurement(self.scale / x.scale)
+        result = UnitOfMeasurement(self.scale / x.scale)
         result.dimensions = dict( (n, self.dimensions.get(n, 0)-x.dimensions.get(n, 0)) 
                                   for n in set(self.dimensions)|set(x.dimensions) )
         return result
     
+    def __eq__(self, x):
+        return self.scale == x.scale and self.dimensions == self.dimensions
     
-    def __string__(self):
+    def __str__(self):
         return "< scale = " + self.scale + " dimensions = " +  self.dimensions +" >"
 
-
-
-
-knownBaseUnits  = {'m': UnitsOfMeasurement(1, {'L': 1}),
-                  'mi': UnitsOfMeasurement(1609.344, {'L': 1}),
-                  's': UnitsOfMeasurement(1, {'T': 1}),
-                  'h': UnitsOfMeasurement(3600, {'T': 1}),
-                  'g': UnitsOfMeasurement(1, {'M': 1}),
-                  'lb': UnitsOfMeasurement(453.59237, {'M': 1}),
-                  'l': UnitsOfMeasurement(0.001, {'L': 3}),
-                  'N': UnitsOfMeasurement(0.001, {'M': 1, 'L': 1, 'T': -2})
-                  }
-
-
-knownDimensions = {'length': {'L':1},
-                   'area': {'L':2},
-                   'volume': {'L':3},
-                   'time': {'T': 1},
-                   'mass': {'M': 1},
-                   'speed': {'T': -1, 'L':1}, 
-                   'acceleration': {'T': -2, 'L':1} 
-                   }
-
-
-knownPrefix = {'p': 10**-12,
-               'n': 10**-9,
-               'u': 10**-6,
-               'm': 10**-3,
-               'k': 10**3,
-               'M': 10**6,
-               'G': 10**9,
-               'T': 10**12,
-               }
+    def isCompatible(self, x):
+        return self.dimensions == x.dimensions
     
-def findKey(dic, val):
-    """return the key of dictionary dic given the value"""
-    return [k for k, v in dic.iteritems() if v == val][0]
-
-
-def parseInput(string): 
+    def dimensionsString(self):
+        abbrev = [k for k, v in knownDimensions.iteritems() if v == self.dimensions]
+        if len(abbrev) == 1:
+            return abbrev[0]
+        else:
+            return str(self.dimensions)
+        
+    def conversionFactor(self, dest):
+        return self.scale / dest.scale
     
-    value = Word(nums).setResultsName("value").setParseAction(lambda tokens: int(tokens[0]))
-    prefix = oneOf("p n u m d h k M G T").setResultsName("prefix")
-    baseUnit = oneOf("m s h l mi in g N").setResultsName("baseUnit")
-    exp = Word(nums).setResultsName("exp").setParseAction(lambda tokens: int(tokens[0]))
-    unitAtom = Combine( ( baseUnit | prefix + baseUnit) + Optional(exp) ).setResultsName("unit")
-    operator = oneOf("* /").setResultsName("operator")
-    unitExpr = Group( unitAtom + ZeroOrMore(operator + unitAtom) )
-    input = value + unitExpr("srcExpr") + "to" + unitExpr("dstExpr")
-    
-    return input.parseString(string, parseAll=True)
-    
-    
-def pairwise(iterable):
-    "s -> (s0,s1), (s2,s3), (s4, s5), ..."
-    a = iter(iterable)
-    return izip(a, a)
- 
-
+  
+  
 def unitFromToken(token):
-    unit = knownBaseUnits[token.baseUnit]
+    unit = getBaseUnit(token.baseUnit)
     
     if token.prefix:
         unit.scale *= knownPrefix[token.prefix]
@@ -120,17 +81,113 @@ def unitFromToken(token):
             unit.dimensions[k] *= token.exp
 
         unit.scale = unit.scale ** token.exp  
-        
     return unit
 
-
+  
 def unitFromExpr(expr):
     result = unitFromToken(expr[0])
-    for operator, token in pairwise(expr[1:]):
-        if operator == "*":
-            result *= unitFromToken(token)
-        else:
-            result /= unitFromToken(token)
+    tokens = iter(expr[1:])
+    for operator, unitToken in izip(tokens, tokens):
+        result = knownOperators[operator]( result,  unitFromToken(unitToken) )
         
     return result
+
+
+knownBaseUnits  = {'1': UnitOfMeasurement(1.0, {}),
+                   'm': UnitOfMeasurement(1.0, {'L': 1}),
+                   'mm': UnitOfMeasurement(0.001, {'L': 1}),
+                   'mi': UnitOfMeasurement(1609.344, {'L': 1}),
+                   'ft': UnitOfMeasurement(0.3048, {'L': 1}),
+                   's': UnitOfMeasurement(1.0, {'T': 1}),
+                   'h': UnitOfMeasurement(3600.0, {'T': 1}),
+                   'Hz': UnitOfMeasurement(1.0, {'T': -1}),
+                   'g': UnitOfMeasurement(1.0, {'M': 1}),
+                   'lb': UnitOfMeasurement(453.59237, {'M': 1}),
+                   'oz': UnitOfMeasurement(28.3495231, {'M': 1}),
+                   'l': UnitOfMeasurement(0.001, {'L': 3}),
+                   'gal': UnitOfMeasurement(0.00378541178, {'L': 3}),
+                   'N': UnitOfMeasurement(0.001, {'M': 1, 'L': 1, 'T': -2}),
+                   'usd': UnitOfMeasurement(1, {'V': 1}),
+                   'eur': UnitOfMeasurement(1.3662, {'V': 1})
+                   }
+
+def getBaseUnit(name):
+    return copy.deepcopy(knownBaseUnits[name])
+
+
+knownDimensions = {'length': {'L':1},
+                   'area': {'L':2},
+                   'volume': {'L':3},
+                   'time': {'T': 1},
+                   'weigth': {'M': 1},
+                   'speed': {'T': -1, 'L':1}, 
+                   'acceleration': {'T': -2, 'L':1} 
+                   }
+
+
+knownPrefix = {'p': 10**-12,
+               'n': 10**-9,
+               'u': 10**-6,
+               'd': 10**-1,
+               'c': 10**-2,
+               'm': 10**-3,
+               'k': 10**3,
+               'M': 10**6,
+               'G': 10**9,
+               'T': 10**12,
+               }
+
+knownOperators = {'*': lambda x,y: x * y, 
+                  '/': lambda x,y: x / y,  
+                  }
+    
+def findKey(dic, val):
+    """return the key of dictionary dic given the value"""
+    return [k for k, v in dic.iteritems() if v == val][0]
+
+
+
+def parseInput(string): 
+    
+    valueExpr = Word(nums + "." + "+" + "-" + "*" + "/" + "(" + ")" ).setResultsName("valueExpr")
+    prefix = oneOf( " ".join(knownPrefix.keys()) ).setResultsName("prefix")
+    baseUnit = oneOf( " ".join(knownBaseUnits.keys()) ).setResultsName("baseUnit")
+    exp = Word(nums).setResultsName("exp").setParseAction(lambda tokens: int(tokens[0]))
+    unitAtom = Combine( ( baseUnit | prefix + baseUnit) + Optional(Optional("^") + exp) ).setResultsName("unit")
+    operator = oneOf(" ".join(knownOperators.keys()) ).setResultsName("operator")
+    unitExpr = Group( unitAtom + ZeroOrMore(operator + unitAtom) )
+    input = valueExpr + unitExpr("srcExpr") + "to" + unitExpr("dstExpr")
+    
+    return input.parseString(string, parseAll=True)
+   
+
+class ConversionResult:
+    def __init__(self):
+        self.__src = None
+        
+        
+def convert(value, srcUnit, dstUnit):    
+    if not srcUnit.isCompatible(dstUnit):
+        raise RuntimeError("Incompatible type " + srcUnit.dimensionsString() + " and " + dstUnit.dimensionsString() )
+    
+    return value * srcUnit.conversionFactor(dstUnit)
+
+
+def convertString(string):
+    res = parseInput(string) 
+    srcUnit = unitFromExpr(res.srcExpr)
+    dstUnit = unitFromExpr(res.dstExpr)
+    try:
+        value = eval(res.valueExpr)
+    except:
+        raise RuntimeError(res.valueExpr + ' is not a valid value')
+    return convert(value, srcUnit, dstUnit)
+
+
+if __name__ == "__main__":
+    args = " ".join(sys.argv[1:])
+    try:
+        print convertString(args)
+    except Exception, e:
+        print e
     
--- src/convertTest.py
+++ src/convertTest.py
@@ -43,27 +43,83 @@ class Test(unittest.TestCase):
         self.assertRaises(ParseException, lambda: parseInput("10 Hl to m3"))
         self.assertRaises(ParseException, lambda: parseInput("10 km3 to Km3"))
 
-    def testParseInput(self):
-        res = parseInput("10 N to kg*m/s2")
-        self.assertEqual(res.value, 10)
- #       self.assertEqual(res.srcUnit.prefix, 'k')
- #       self.assertEqual(res.srcUnit.basicUnit, 'm')
- #       self.assertEqual(res.dstUnit.basicUnit, 'mi')
         
     
     def testUnitFromExpr(self):
         res = parseInput("10 N to kg*m/s2")
-
+        
         srcUnit = unitFromExpr(res.srcExpr)
         dstUnit = unitFromExpr(res.dstExpr)
         
         self.assertEqual(srcUnit.scale, 0.001)
-        self.assertEqual(dstUnit.dimensions, {'M':1, 'L':1, 'T':-2})
+        self.assertEqual(srcUnit.dimensions, {'M':1, 'L':1, 'T':-2})
         self.assertEqual(dstUnit.scale, 1000)
         self.assertEqual(dstUnit.dimensions, {'M':1, 'L':1, 'T':-2})
         
-
+   
+    def testUnitFromExpr2(self):     
+        res = parseInput("1 m to m2")
         
+        srcUnit = unitFromExpr(res.srcExpr)
+        dstUnit = unitFromExpr(res.dstExpr)
+        
+        self.assertEqual(srcUnit.scale, 1)
+        self.assertEqual(srcUnit.dimensions, {'L':1})
+        self.assertEqual(dstUnit.scale, 1)
+        self.assertEqual(dstUnit.dimensions, {'L':2})
+        
+        
+    def testUnitFromExpr3(self):     
+        res = parseInput("1 ft to mi2")
+        
+        srcUnit = unitFromExpr(res.srcExpr)
+        dstUnit = unitFromExpr(res.dstExpr)
+        
+        self.assertEqual(srcUnit.scale, 0.3048)
+        self.assertEqual(srcUnit.dimensions, {'L':1})
+        self.assertEqual(dstUnit.scale, 1609.344 ** 2)
+        self.assertEqual(dstUnit.dimensions, {'L':2})
+        
+
+    def testUnitOfMeasurement(self):
+        unit1 = UnitOfMeasurement(12, {'L':3, 'T':-2})
+        unit2 = UnitOfMeasurement(2, {'M':2, 'L':-1})
+        self.assertNotEqual(unit1, unit2)
+        
+        unit3 = unit1 * unit2
+        self.assertEqual(unit3, UnitOfMeasurement(24, {'L':2, 'T':-2, 'M':2}) )
+    
+        unit3 = unit1 / unit2
+        self.assertEqual(unit3, UnitOfMeasurement(6, {'L':4, 'T':-2, 'M':-2}) )
+
+        self.assertEqual(UnitOfMeasurement(12, {'L':1}).dimensionsString(), 'length')
+        self.assertEqual(UnitOfMeasurement(12, {'L':1, 'T':-2}).dimensionsString(), 'acceleration')
+
+        self.assertFalse(unit1.isCompatible(unit2))
+        self.assertFalse(unit2.isCompatible(unit1))
+        
+        unit2 = UnitOfMeasurement(2, {'L':3, 'T':-2})
+        self.assertTrue(unit1.isCompatible(unit2))
+        self.assertTrue(unit2.isCompatible(unit1))
+        
+        self.assertEqual(unit1.conversionFactor(unit2), 6)
+        self.assertEqual(unit2.conversionFactor(unit1), 2/12)
+
+    
+    def testConvert(self):
+        self.assertRaises(RuntimeError, lambda: convertString("1 m to m2"))
+        
+    def testConvert2(self):
+        self.assertEqual(convertString("1 m to cm"), 100)
+        self.assertEqual(convertString("1 m to mm"), 1000)
+        self.assertEqual(convertString("1 ft/s to km/h"), 1.09728)
+        self.assertAlmostEqual(convertString("1 ft/s2 to km/s2"), 0.0003048, delta=0.00000001)
+        self.assertEqual(convertString("1 m2 to cm2"), 10000)
+        self.assertAlmostEqual(convertString("1 m3 to mm3"), 1000000000, delta=0.001)
+        self.assertEqual(convertString("10 1/s to Hz"), 10)
+        self.assertAlmostEqual(convertString("3.6 usd/gal to eur/l"), 0.696105540546, delta=0.00000001)
+
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testParseInput']
